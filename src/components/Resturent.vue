fc202518c1a2dadb3020babbef1f69c763e0315a
tapash22 <tapasp263@gmail.com>
2023-01-01 20:00:39 +0600

    add resturent details

--- src/components/Resturent.vue
+++ src/components/Resturent.vue
@@ -3,9 +3,9 @@
         <router-link :to="{ name: 'restaurantdetails', params: { id: resturent.id } }" class="text-decoration-none">
             <v-card>
                 <v-img width="250" height="200" :src="resturent.image">
-                    <v-chip color="red" class="white--text">promo</v-chip>
-                    <v-card-title class="d-flex justify-end red--text white lighten-1 pa-1 text-body-2 w-25">promo
-                        code</v-card-title>
+
+                    <v-card-title class="d-flex justify-end red--text white lighten-1 pa-1 text-body-2">
+                        <v-chip color="red" class="white--text text-body-1">promo</v-chip> </v-card-title>
                     <v-card-text class="red lighten-2 pa-0 ma-0 card-text">
                         <v-card-title class="pa-0 text-body-1 mx-2 text-uppercase my-2">{{ resturent.name
                             }}</v-card-title>
@@ -13,7 +13,6 @@
                 </v-img>
             </v-card>
         </router-link>
-
     </div>
 </template>
 
--- src/views/RestaurantDetails.vue
+++ src/views/RestaurantDetails.vue
@@ -1,24 +1,44 @@
 <template>
     <div class="resturent-details">
-{{ resturent }}
+        <h2 class="text-center my-2 text-h6">Resturent Details</h2>
+        <v-row class="d-flex justify-center my-2">
+            <v-col cols="12" sm="12" md="4" lg="4" xl="4">
+                <v-img :src="resturent.image" />
+                <v-rating value="4" dense color="red" class="my-2" />
+                <v-sheet elevation="1" rounded="lg" class="d-flex justify-space-between">
+                    <h2 class="text-h6 mx-2 my-8">Address:</h2>
+                    <p class="text-body-1 pa-3 text-justify address">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda iure eligendi cupiditate.
+                        Eos
+                        ut saepe libero corporis, iusto natus aspernatur ex reprehenderit hic recusandae aperiam porro
+                        iste ad suscipit!
+                    </p>
+                </v-sheet>
+            </v-col>
+            <v-col cols="12" sm="12" md="6" lg="6" xl="6">
+                <v-card>
+                    <v-card-title>{{ resturent.name }}</v-card-title>
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 export default {
-    name:'resturent-details',
-    data(){
-        return{
-            id:this.$route.params.id
+    name: 'resturent-details',
+    data() {
+        return {
+            id: this.$route.params.id
         }
     },
-    computed:{
-        resturent(){
+    computed: {
+        resturent() {
             return this.getResturentById(this.id);
         },
-        ...mapGetters('resturents',{
-            getResturentById:'getResturentById'
+        ...mapGetters('resturents', {
+            getResturentById: 'getResturentById'
         })
     }
 
