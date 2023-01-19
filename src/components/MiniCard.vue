8cf72274fb969cad77aeabd5b5dcb376bdab69b1
tapash22 <tapasp263@gmail.com>
2023-01-19 17:09:58 +0600

    update mini card

--- src/components/MiniCard.vue
+++ src/components/MiniCard.vue
@@ -1,42 +1,49 @@
 <template>
-    <v-card class="mx-0" width="250px">
-        <v-card-text >
-            <v-list class="pa-0">
-                <v-list-item v-for="item in cart" :key="item.product.id" class="pa-0">
-                    <v-list-item-content >
-                        <v-list-item-title>{{ item.product.name }}</v-list-item-title>
-                        <v-list-item-subtitle>{{ item.quantity + 'X' + item.product.price }}</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-list-item-action>
-                        <a @click.prevent="removeProductFromCart(item.product)">
-                            <v-icon color="grey lighten-1">mdi-delete</v-icon>
-                        </a>
+<v-card class="mx-0 pa-0" width="200px">
+    <v-card-text class=" d-flex justify-space-between">
+        <v-card-title class="d-flex justify-start text-body-1 pa-0 font-weight-bold ">Place Order</v-card-title>
+        <v-btn color="green" small text class="text-body-2 black--text" @click.prevent="clearCardItem()">
+            clear
+        </v-btn>
+    </v-card-text>
+    <v-divider />
 
-                    </v-list-item-action>
-                    <hr />
-                </v-list-item>
-                <v-list-item class="d-flex justify-space-between">
-                    <v-list-item-title>total: ${{ cardTotalPrice }}</v-list-item-title>
+    <v-card-text class="pa-0">
+        <v-list class="mx-2 pa-0">
+            <v-list-item v-for="item in cart" :key="item.product.id" class="pa-0">
+                <v-list-item-content>
+                    <v-list-item-title>{{ item.product.name }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ item.quantity + 'X' + item.product.price }}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                    <a @click.prevent="removeProductFromCart(item.product)">
+                        <v-icon color="grey lighten-1">mdi-delete</v-icon>
+                    </a>
 
-                    <v-btn color="primary " text @click.prevent="clearCardItem()">
-                        clear
-                    </v-btn>
+                </v-list-item-action>
+                <hr />
+            </v-list-item>
+            <v-list-item class="d-flex justify-center pa-0 ">
+                <v-list-item-title class="text-body-1 pa-0 font-weight-bold">Total: ${{ cardTotalPrice }}</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-card-text>
 
-                </v-list-item>
-            </v-list>
-        </v-card-text>
-        <v-card-actions class="d-block  w-100 pa-0">
-            <router-link :to="{ name: 'checkout' }" class="text-decoration-none">
-                <v-btn small color="info" dark width="100%" class="w-100">
-                    Checkout
-                </v-btn>
-            </router-link>
-        </v-card-actions>
-    </v-card>
+    <v-card-actions class="pa-0 my-2 d-flex justify-center">
+        <router-link :to="{name:'checkout'}" class="text-decoration-none pa-0 w-100 ">
+            <v-btn small color="green lignten-5" width="100%" class="black--text text-body-1" text>
+                Checkout
+            </v-btn>
+        </router-link>
+    </v-card-actions>
+</v-card>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex'
+import {
+    mapGetters,
+    mapState
+} from 'vuex'
 export default {
     name: 'mini-cart',
     computed: {
